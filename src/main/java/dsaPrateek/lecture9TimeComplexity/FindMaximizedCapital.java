1700e76c31732ed60d0829b31d9ddf1c51cd5a76
Arnab Biswas <arnabbiswas@yahoo.com>
2021-07-04 10:12:58 -0400

    Miscellaneous files

---
+++ src/main/java/dsaPrateek/lecture9TimeComplexity/FindMaximizedCapital.java
@@ -0,0 +1,31 @@
+package dsaPrateek.lecture9TimeComplexity;
+
+import java.util.PriorityQueue;
+
+public class FindMaximizedCapital {
+    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        PriorityQueue<int[]> pqCap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        PriorityQueue<int[]> pqPro = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
+
+        for (int i = 0; i < Profits.length; i++) {
+            pqCap.add(new int[] {Capital[i], Profits[i]});
+        }
+
+        for (int i = 0; i < k; i++) {
+            while (!pqCap.isEmpty() && pqCap.peek()[0] <= W) {
+                pqPro.add(pqCap.poll());
+            }
+
+            if (pqPro.isEmpty()) break;
+
+            W += pqPro.poll()[1];
+        }
+
+        return W;
+    }
+
+    public static void main(String[] args) {
+        FindMaximizedCapital mc = new FindMaximizedCapital();
+        System.out.println(mc.findMaximizedCapital(3,0, new int[]{1, 2, 3}, new int[]{0, 1, 2}));
+    }
+}
